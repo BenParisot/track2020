5e4fdb8e79e3f0ea21b598cdeaeaa8c3afb058d0
Ben Parisot <me@benparisot.com>
2019-05-19 11:55:43 -0700

    express app to new file, import and confirm server still running and receiving requests correctly

--- server/server.js
+++ server/server.js
@@ -1,16 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
-
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+const app = require('../lib/app');
 
 app.listen(7890, () => {
   console.log('server running on port 7890');
