40280142c4570bb6a260102c0756d4eb97e019d6
Ben Parisot <me@benparisot.com>
2019-05-19 15:32:14 -0700

    get user creation working

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 require('../lib/utils/connect')();
 const app = require('../lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 4242;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
